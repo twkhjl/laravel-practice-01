738b6dd781865765b4f3cd11e84229824e60466f
twkhjl <twkhjl@gmail.com>
2022-08-10 22:47:38 +0800

    add layout

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,9 +1,14 @@
-@if ($listing == null)
-    <h3>id: {{ $id }} does not exists</h3>
-@else
-    <h1>{{ $listing['title'] }}</h1>
-    <div>{{ $listing['description'] }}</div>
+@extends('layout')
 
 
-    <h3><a href={{ route('listings.index') }}>back</a></h3>
-@endif
+@section('content')
+    @if ($listing == null)
+        <h3>id: {{ $id }} does not exists</h3>
+    @else
+        <h1>{{ $listing['title'] }}</h1>
+        <div>{{ $listing['description'] }}</div>
+
+
+        <h3><a href={{ route('listings.index') }}>back</a></h3>
+    @endif
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,13 +1,22 @@
-<h1>{{ $header }}</h1>
+@extends('layout')
 
 
-@unless(count($listings) == 0)
+@section('content')
 
-    @foreach ($listings as $listing)
-        <a href={{ route('listings.show',['id'=>$listing['id']]) }}><h2>{{ $listing['title'] }}</h2></a>
+    <h1>{{ $header }}</h1>
 
-        <div>{{ $listing['description'] }}</div>
-    @endforeach
-@else
-    <div>no listings found...</div>
-@endunless
+
+    @unless(count($listings) == 0)
+        @foreach ($listings as $listing)
+            <a href={{ route('listings.show', ['id' => $listing['id']]) }}>
+                <h2>{{ $listing['title'] }}</h2>
+            </a>
+
+            <div>{{ $listing['description'] }}</div>
+        @endforeach
+    @else
+        <div>no listings found...</div>
+    @endunless
+
+
+@endsection
