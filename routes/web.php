fd30b65efe3825fdab6daaeec711aad156622550
twkhjl <twkhjl@gmail.com>
2022-09-11 22:00:23 +0800

    add auth middleware to route group

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 
 Route::prefix('listings')->group(function () {
 
-    Route::get('/', [ListingController::class,'index'])->name('listings.index');
+    Route::get('/', [ListingController::class, 'index'])->name('listings.index');
 
     // Route::get('/id/{id}', function ($id) {
     //     // dd(Listing::find($id));
@@ -61,30 +61,27 @@ Route::prefix('listings')->group(function () {
 
     // ===================================
 
-    Route::get('/id/{listing}',[ListingController::class,"show"])->name('listings.show');
-    Route::get('/create',[ListingController::class,"create"])->name('listings.create');
-    Route::post('/store',[ListingController::class,"store"])->name('listings.store');
-    Route::get('/id/{listing}/edit',[ListingController::class,"edit"])->name('listings.edit');
-    Route::post('/id/{listing}/update',[ListingController::class,"update"])->name('listings.update');
-    Route::delete('/id/{listing}/delete',[ListingController::class,"destroy"])->name('listings.destroy');
+    Route::get('/id/{listing}', [ListingController::class, "show"])->name('listings.show');
 
-    Route::get('/user/{user_id}/manage',[ListingController::class,"manage"])->name('listings.manage');
+    Route::group(['middleware' => 'auth'], function () {
 
+        Route::get('/create', [ListingController::class, "create"])->name('listings.create');
+        Route::post('/store', [ListingController::class, "store"])->name('listings.store');
+        Route::get('/id/{listing}/edit', [ListingController::class, "edit"])->name('listings.edit');
+        Route::post('/id/{listing}/update', [ListingController::class, "update"])->name('listings.update');
+        Route::delete('/id/{listing}/delete', [ListingController::class, "destroy"])->name('listings.destroy');
+        Route::get('/user/{user_id}/manage', [ListingController::class, "manage"])->name('listings.manage');
 
-
+    });
 });
 
 
 Route::prefix('users')->group(function () {
 
-    Route::get('/register',[UserController::class,"register"])->name('users.register');
-    Route::post('/store',[UserController::class,"store"])->name('users.store');
+    Route::get('/register', [UserController::class, "register"])->name('users.register');
+    Route::post('/store', [UserController::class, "store"])->name('users.store');
 
-    Route::post('/logout',[UserController::class,"logout"])->name('users.logout');
-    Route::get('/login',[UserController::class,"login"])->name('users.login');
-    Route::post('/authenticate',[UserController::class,"authenticate"])->name('users.authenticate');
-
-
+    Route::post('/logout', [UserController::class, "logout"])->name('users.logout');
+    Route::get('/login', [UserController::class, "login"])->name('users.login');
+    Route::post('/authenticate', [UserController::class, "authenticate"])->name('users.authenticate');
 });
-
-
